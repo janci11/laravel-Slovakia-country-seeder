f4feff57d8e08f290971d2b2cf1c0be0607ce407
Ján Pažitný <pazitny.jan11@gmail.com>
2024-01-22 15:21:37 +0100

    Update DatabaseSeeder.php
    Correction for semicolons in run method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CountriesTableSeeder::class),
-        $this->call(RegionsTableSeeder::class),
-        $this->call(DistrictsTableSeeder::class),
-        $this->call(MunicipalityTypesTableSeeder::class),
-        $this->call(MunicipalitiesTableSeeder::class)
+        $this->call(CountriesTableSeeder::class);
+        $this->call(RegionsTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
+        $this->call(MunicipalityTypesTableSeeder::class);
+        $this->call(MunicipalitiesTableSeeder::class);
     }
 }
